ac8ea8465d13d222e0091145ed055f948fcc44ce
s1f102300571 <s1f102300571@users.noreply.github.com>
2023-05-06 10:30:00 +0000

    deploy: f415ed53e3496df8eab441d58b77fc63892cbd2d

---
+++ _next/static/SAP9U8lnDd7c0Tm5WPpq9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3ff45f1a2ee7d733.css","static/chunks/pages/index-8da3b2e85058e538.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
