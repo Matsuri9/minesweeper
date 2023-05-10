0187cbade28aae4a6a6511236017388107072ebe
s1f102300571 <s1f102300571@users.noreply.github.com>
2023-05-10 01:25:41 +0000

    deploy: 03379789536903b2f3488b99a13c777702103881

---
+++ _next/static/taeMM_20YMZ11GQ9wIBQv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/68d08fd09f921934.css","static/chunks/pages/index-8e25bac83087ec35.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
